cd98a78bb0096de2766c68dc30f82f4bbee9127b
fiko942 <tobellord@gmail.com>
2025-03-13 21:53:20 +0700

    add Container component and update layout in _app.jsx; modify homepage title

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import "../styles/globals.css"; // Pastikan ada file CSS untuk styling global
+import Container from "../component/Container";
 
 export default function MyApp({ Component, pageProps }) {
     return (
@@ -7,7 +8,9 @@ export default function MyApp({ Component, pageProps }) {
             <Head>
                 <title>Wiji's Portfolio</title>
             </Head>
-            <Component {...pageProps} />
+            <Container>
+                <Component {...pageProps} />
+            </Container>
         </>
     );
 }
